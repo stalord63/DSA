3768b7318216678230c9b97543ec00839bc1cb68
stalord63 <adi06031998@gmail.com>
2021-05-23 00:36:50 +0530

    recursion

---
+++ GEEKSFORGEEKS/recursion/subset.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void subset(string str,string curr,int i){
+if(i==str.length())
+{cout<<curr<<endl;
+return; }
+
+subset(str,curr,i+1);
+subset(str,curr+str[i],i+1);}
+
+
+
+
+
+
+
+int main(){
+
+string str;
+
+cin>>str;
+
+subset(str," ",0);
+return 0;
+
+
+
+
+}
