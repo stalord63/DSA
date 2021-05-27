9f3c93c76715335351748f20c24d03af9435088c
stalord63 <adi06031998@gmail.com>
2021-05-27 23:35:28 +0530

    first cheat

--- GEEKSFORGEEKS/searching/binarysearcrecusive.cpp
+++ GEEKSFORGEEKS/searching/binarysearcrecusive.cpp
@@ -1,34 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int bs(int *arr,int low,int high,int x)
-{
-if(low>high){
-return -1;}
 
-
-int mid=(low+high) / 2;
-
-if(arr[mid]==x){
-return mid;}
-
-else if(arr[mid]>x){
-bs(arr,low,mid-1,x);}
-else {bs(arr,mid+1,high,x);}
-}
 int main()
 {
-int a[100];
-int n;
-cin>>n;
-int x;
-cin>>x;
-for(int i=0;i<n;i++){
-    cin>>a[i];
-}
-
-int low=0;
-int high=n-1;
-int c=bs(a,low,high,x);
-cout<<c;
+int a;int b;
+cin>>a>>b;
+int sum=a+b;
+cout<<sum;
 return 0;
 }
