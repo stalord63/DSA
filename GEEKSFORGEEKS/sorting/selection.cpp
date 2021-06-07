b3c73f7968d2ae43fd7e80ff8170dca2f63d0024
stalord63 <adi06031998@gmail.com>
2021-06-07 20:21:53 +0530

    sorting

---
+++ GEEKSFORGEEKS/sorting/selection.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    
+    void selectionSort(int a[], int n)
+    {
+        
+       for(int  i=0;i<n;i++){
+           int mid=i;
+           for(int j=i+1;j<n;j++){
+               if(a[j]<a[mid]){
+               mid=j;
+             
+           }
+           
+           
+           }
+             swap(&a[i],&a[mid]);
+           
+           
+           
+       }
+    }
+};
+
+// { Driver Code Starts.
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    Solution ob;  
+    ob.selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}  // } Driver Code Ends
